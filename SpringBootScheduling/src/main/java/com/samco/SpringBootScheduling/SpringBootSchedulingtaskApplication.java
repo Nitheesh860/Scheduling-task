fb6cf50d591f4312307a8c6cc2031193e40aeb73
Nitheesh <42829091+Nitheesh860@users.noreply.github.com>
2022-09-08 15:47:02 +0530

    Add files via upload

---
+++ SpringBootScheduling/src/main/java/com/samco/SpringBootScheduling/SpringBootSchedulingtaskApplication.java
@@ -0,0 +1,13 @@
+package com.samco.SpringBootScheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSchedulingtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSchedulingtaskApplication.class, args);
+	}
+
+}
---
+++ SpringBootScheduling/src/test/java/com/samco/SpringBootScheduling/SpringBootSchedulingtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samco.SpringBootScheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSchedulingtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
